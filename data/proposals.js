fa8b577e8e8f1321ce75f45f47d26c3d69c0d099
kunaltyagi <tyagi.kunal@live.com>
2014-02-20 14:43:44 +0530

    done, cleared muck

--- data/proposals.js
+++ data/proposals.js
@@ -1,22 +1,18 @@
 // Add your proposal to the list, but please, 
 // remember about alphabetical order. If your show is
-// already on the list, just put your github account
+// already on the list, just put your github account username
 // in the 'votes' part. Thanks!
+//
+// Some changes in Nomenclature:
+// Do make episodes = totalEpisodes
+// stillWatching is sugar for Ongoing
 
 var toConsider = [{
-  "title": "Sample",
-  "imdb": "http://www.imdb.com/title/tt00000",
-  "episodes": 1,
-  "totalEpisodes": 12,
-  "episodeLength": 23,
-  "stillWatching": 1,
-  "votes": ["github_account","another_gh_account"]
-}, {
   "title": "Joey",
   "imdb": "http://www.imdb.com/title/tt0786156/",
-  "episodes": 1,
+  "episodes": 46,
   "totalEpisodes": 46,
   "episodeLength": 30,
   "stillWatching": 0,
-  "votes": ["github_account","another_gh_account"]
+  "votes": ["bhaskarsuper9000"]
 }]
